8673a1ea79157506e92fea2deafb874ef0764de4
wawan133 <prastyowawan0@gmail.com>
2021-05-20 14:34:57 +0700

    perubahan

--- application/views/components/foot.php
+++ application/views/components/foot.php
@@ -1,8 +1,8 @@
 <!--Footer-part-->
 
 <div class="row-fluid">
-	<div id="footer" class="span12"> 2019 &copy; Pembelajaran Online <a
-			href="home">KELOMPOK3</a> - Design by Al azmi</div>
+	<div id="footer" class="span12"> 2021 &copy; Pembelajaran Online <a
+			href="home">SISFO</a> -RUANG MURID</div>
 </div>
 
 <!--end-Footer-part-->
